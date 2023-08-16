f510203a02ac10462e6a526e61044db82105e51a
Vitali Varabuey <72112909+Vorobey1992@users.noreply.github.com>
2023-08-16 12:13:55 +0300

    Add project files.

---
+++ DataAccessLibrary/OrderRepository.cs
@@ -0,0 +1,166 @@
+﻿using ADO.NET_Fundamentals.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO.NET_Fundamentals.Data
+{
+    public class OrderRepository : IDisposable
+    {
+        private readonly string _connectionString;
+        private SqlConnection _connection;
+
+        public OrderRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+            _connection = new SqlConnection(_connectionString);
+            _connection.Open();
+        }
+
+        // CRUD operations for Order
+        public int CreateOrder(Order order)
+        {
+            using SqlConnection connection = new(_connectionString);
+            connection.Open();
+
+            string insertOrderQuery = @"
+                    INSERT INTO Orders (Status, CreatedDate, UpdatedDate, ProductId)
+                    VALUES (@Status, @CreatedDate, @UpdatedDate, @ProductId)
+                    SELECT SCOPE_IDENTITY();
+                ";
+
+            using SqlCommand command = new(insertOrderQuery, connection);
+            command.Parameters.AddWithValue("@Status", order.Status);
+            command.Parameters.AddWithValue("@CreatedDate", order.CreatedDate);
+            command.Parameters.AddWithValue("@UpdatedDate", order.UpdatedDate);
+            command.Parameters.AddWithValue("@ProductId", order.ProductId);
+
+            // Выполняем команду и получаем Id с помощью ExecuteScalar
+            int newOrderId = Convert.ToInt32(command.ExecuteScalar());
+            return newOrderId;
+        }
+
+        public void UpdateOrder(int orderId, Order order)
+        {
+            using SqlConnection connection = new(_connectionString);
+            connection.Open();
+
+            string updateOrderQuery = @"
+                    UPDATE Orders
+                    SET Status = @Status, CreatedDate = @CreatedDate, UpdatedDate = @UpdatedDate, ProductId = @ProductId
+                    WHERE Id = @Id
+                ";
+
+            using SqlCommand command = new(updateOrderQuery, connection);
+            command.Parameters.AddWithValue("@Id", orderId);
+            command.Parameters.AddWithValue("@Status", order.Status);
+            command.Parameters.AddWithValue("@CreatedDate", order.CreatedDate);
+            command.Parameters.AddWithValue("@UpdatedDate", order.UpdatedDate);
+            command.Parameters.AddWithValue("@ProductId", order.ProductId);
+
+            command.ExecuteNonQuery();
+        }
+
+        public void DeleteOrder(int orderId)
+        {
+            using SqlConnection connection = new(_connectionString);
+            connection.Open();
+
+            string deleteOrderQuery = @"
+                    DELETE FROM Orders
+                    WHERE Id = @Id
+                ";
+
+            using SqlCommand command = new(deleteOrderQuery, connection);
+            command.Parameters.AddWithValue("@Id", orderId);
+            command.ExecuteNonQuery();
+        }
+
+        public List<Order> GetOrdersByFilter(DateTime startDate, DateTime endDate, OrderStatus status, int productId)
+        {
+            List<Order> orders = new();
+
+            using (SqlConnection connection = new(_connectionString))
+            {
+                connection.Open();
+
+                string getOrdersByFilterQuery = @"
+                    SELECT Id, Status, CreatedDate, UpdatedDate, ProductId
+                    FROM Orders
+                    WHERE CreatedDate >= @StartDate AND CreatedDate <= @EndDate AND Status = @Status AND ProductId = @ProductId
+                ";
+
+                using SqlCommand command = new(getOrdersByFilterQuery, connection);
+                command.Parameters.AddWithValue("@StartDate", startDate);
+                command.Parameters.AddWithValue("@EndDate", endDate);
+                command.Parameters.AddWithValue("@Status", status);
+                command.Parameters.AddWithValue("@ProductId", productId);
+
+                using SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    Order order = new()
+                    {
+                        Id = Convert.ToInt32(reader["Id"]),
+                        Status = Enum.Parse<OrderStatus>(reader["Status"].ToString()), // Преобразуем строку в перечисление
+                        CreatedDate = Convert.ToDateTime(reader["CreatedDate"]),
+                        UpdatedDate = Convert.ToDateTime(reader["UpdatedDate"]),
+                        ProductId = Convert.ToInt32(reader["ProductId"])
+                    };
+                    orders.Add(order);
+                }
+            }
+
+            return orders;
+        }
+
+        public void BulkDeleteOrdersByFilter(DateTime startDate, DateTime endDate, OrderStatus status, int productId)
+        {
+            using SqlConnection connection = new(_connectionString);
+            connection.Open();
+
+            SqlTransaction transaction = connection.BeginTransaction(); // Начало транзакции
+
+            try
+            {
+                string bulkDeleteOrdersQuery = @"
+            DELETE FROM Orders
+            WHERE CreatedDate >= @StartDate AND CreatedDate <= @EndDate AND Status = @Status AND ProductId = @ProductId
+        ";
+
+                using SqlCommand command = new(bulkDeleteOrdersQuery, connection);
+                command.Parameters.AddWithValue("@StartDate", startDate);
+                command.Parameters.AddWithValue("@EndDate", endDate);
+                command.Parameters.AddWithValue("@Status", status);
+                command.Parameters.AddWithValue("@ProductId", productId);
+
+                // Устанавливаем транзакцию для команды
+                command.Transaction = transaction;
+
+                command.ExecuteNonQuery();
+
+                // Если все операции успешно выполнены, фиксируем транзакцию
+                transaction.Commit();
+            }
+            catch (Exception)
+            {
+                // В случае ошибки, выполняем роллбек, чтобы отменить все изменения
+                transaction.Rollback();
+                throw; // Передаем ошибку дальше
+            }
+        }
+
+        public void Dispose()
+        {
+            if (_connection != null)
+            {
+                _connection.Close();
+                _connection.Dispose();
+                _connection = null;
+            }
+        }
+    }
+}
---
+++ DataAccessLibrary/ProductRepository.cs
@@ -0,0 +1,131 @@
+﻿using ADO.NET_Fundamentals.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO.NET_Fundamentals.Data
+{
+    public class ProductRepository : IDisposable
+    {
+        private readonly string _connectionString;
+        private SqlConnection _connection;
+
+        public ProductRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+            _connection = new SqlConnection(_connectionString);
+            _connection.Open();
+        }
+
+        // CRUD operations for Product
+        public int CreateProduct(Product product)
+        {
+            using SqlConnection connection = new(_connectionString);
+            connection.Open();
+
+            string insertProductQuery = @"
+                INSERT INTO Products (Name, Description, Weight, Height, Width, Length)
+                VALUES (@Name, @Description, @Weight, @Height, @Width, @Length);
+                SELECT SCOPE_IDENTITY();
+                                                        ";
+
+            using SqlCommand command = new(insertProductQuery, connection);
+            command.Parameters.AddWithValue("@Name", product.Name);
+            command.Parameters.AddWithValue("@Description", product.Description);
+            command.Parameters.AddWithValue("@Weight", product.Weight);
+            command.Parameters.AddWithValue("@Height", product.Height);
+            command.Parameters.AddWithValue("@Width", product.Width);
+            command.Parameters.AddWithValue("@Length", product.Length);
+
+            // Выполняем команду и получаем Id с помощью ExecuteScalar
+            int newProductId = Convert.ToInt32(command.ExecuteScalar());
+            return newProductId;
+
+        }
+
+        public void UpdateProduct(int productId, Product updatedProduct)
+        {
+            using SqlConnection connection = new(_connectionString);
+            connection.Open();
+
+            string updateProductQuery = @"
+                    UPDATE Products
+                    SET Name = @Name, Description = @Description, Weight = @Weight, Height = @Height, Width = @Width, Length = @Length
+                    WHERE Id = @Id
+                                     ";
+
+            using SqlCommand command = new(updateProductQuery, connection);
+            command.Parameters.AddWithValue("@Id", productId); // Используем переданный Id
+            command.Parameters.AddWithValue("@Name", updatedProduct.Name);
+            command.Parameters.AddWithValue("@Description", updatedProduct.Description);
+            command.Parameters.AddWithValue("@Weight", updatedProduct.Weight);
+            command.Parameters.AddWithValue("@Height", updatedProduct.Height);
+            command.Parameters.AddWithValue("@Width", updatedProduct.Width);
+            command.Parameters.AddWithValue("@Length", updatedProduct.Length);
+
+            command.ExecuteNonQuery();
+        }
+
+
+        public void DeleteProduct(int productId)
+        {
+            using SqlConnection connection = new(_connectionString);
+            connection.Open();
+
+            string deleteProductQuery = @"
+                    DELETE FROM Products
+                    WHERE Id = @Id
+                ";
+
+            using SqlCommand command = new(deleteProductQuery, connection);
+            command.Parameters.AddWithValue("@Id", productId);
+            command.ExecuteNonQuery();
+        }
+
+        public List<Product> GetAllProducts()
+        {
+            List<Product> products = new();
+
+            using (SqlConnection connection = new(_connectionString))
+            {
+                connection.Open();
+
+                string getAllProductsQuery = @"
+                    SELECT Id, Name, Description, Weight, Height, Width, Length
+                    FROM Products
+                ";
+
+                using SqlCommand command = new(getAllProductsQuery, connection);
+                using SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    Product product = new()
+                    {
+                        Id = Convert.ToInt32(reader["Id"]),
+                        Name = reader["Name"].ToString(),
+                        Description = reader["Description"].ToString(),
+                        Weight = Convert.ToDouble(reader["Weight"]),
+                        Height = Convert.ToDouble(reader["Height"]),
+                        Width = Convert.ToDouble(reader["Width"]),
+                        Length = Convert.ToDouble(reader["Length"])
+                    };
+                    products.Add(product);
+                }
+            }
+
+            return products;
+        }
+        public void Dispose()
+        {
+            if (_connection != null)
+            {
+                _connection.Close();
+                _connection.Dispose();
+                _connection = null;
+            }
+        }
+    }
+}
